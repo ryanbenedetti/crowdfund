bf2188035d3bf2102c0041c9cb1d7a70af9dc436
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-08 16:31:37 -0700

    Created crowdfund app as a ride-along with Pragmatic Studios RoR course

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,8 @@
+class ProjectsController < ApplicationController
+  def index
+  @projects = Project.all  
+  end
+end
+
+#iteration 1: Hard-coded array 
+#@projects = ["Beer-bot","Ants versus Mambas Game","Conference Kayak"]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdfund::Application.config.secret_key_base = '367e8003b553ead97d2e64a63d785746da01c0923cc36c00c3b3b8d0489059fd7a822027762fb4eb26fbc5153a2bf8e124e8c23d667ad2509ac040f01213c45c'
