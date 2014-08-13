21ab6742896324d283489969696f9bb201668d0f
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-13 15:55:14 -0700

    Routes -> Show page complete.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,11 @@ class ProjectsController < ApplicationController
   def index
   @projects = Project.all
   end
+  
+  def show
+  @project = Project.find(params[:id])
+  end
+  
 end
 
 #iteration 1: Hard-coded array 
